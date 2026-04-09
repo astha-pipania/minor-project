c132f8f3840e03fe575ab0f18de705c26533d23f
astha-pipania <asthapipania@gmail.com>
2026-04-09 23:13:50 +0530

    fixed submodule issue

---
+++ Sports-Dashboard/README.md
@@ -0,0 +1,3 @@
+# Sports-Dashboard
+ Building a Responsive Sports Dashboard using HTML, CSS, and JavaScript
+![Sports Dashboard ](https://github.com/DhruvinBhalala/Sports-Dashboard/assets/142414337/6df6241a-0f96-4765-b1d5-ebda851fe77d)
---
+++ Sports-Dashboard/app.js
@@ -0,0 +1,13 @@
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach((item, j) => {
+      item.className = "nav-item";
+    });
+    navItem.className = "nav-item active";
+  });
+});
+function loadPage(page){
+    document.getElementById("mainFrame").src = page;
+}
